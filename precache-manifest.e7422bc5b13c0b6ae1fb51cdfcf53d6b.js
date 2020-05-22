98cc5601150370f74ea299dc9b7acf5c6a883dc4
Vadym <shelbyua17@gmail.com>
2020-05-22 14:05:11 +0300

    Updates

---
+++ precache-manifest.e7422bc5b13c0b6ae1fb51cdfcf53d6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686f915a90e48f759b7050f496750243",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "80a62249f714e7fea6d2",
+    "url": "/react-tic-tac-toe/static/css/main.7474b60d.chunk.css"
+  },
+  {
+    "revision": "cb7c0b5ec2fbf82a3e62",
+    "url": "/react-tic-tac-toe/static/js/2.fe0f7632.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.fe0f7632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80a62249f714e7fea6d2",
+    "url": "/react-tic-tac-toe/static/js/main.db1f5be8.chunk.js"
+  },
+  {
+    "revision": "4eca8d306c2d5f6566de",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.d2cea36d.js"
+  }
+]);
